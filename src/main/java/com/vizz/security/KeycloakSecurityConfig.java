4cf062ce91a04ae9458a7028f00eb8d3177e35ce
VijayHYadav <vizzyadav1997@gmail.com>
2022-08-03 02:58:26 +0530

    2. Keycloak Configuration with Spring Security.

---
+++ src/main/java/com/vizz/security/KeycloakSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.vizz.security;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+    
+    @Override
+    protected void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws java.lang.Exception {
+        super.configure(http);
+        http.authorizeRequests().antMatchers("contact-us").permitAll()
+        .anyRequest().authenticated()
+        .and().exceptionHandling().accessDeniedPage("/access-denied");
+    }
+}
--- src/main/java/com/vizz/security/SpringSecurityConfig.java
+++ src/main/java/com/vizz/security/SpringSecurityConfig.java
@@ -1,42 +1,42 @@
-package com.vizz.security;
+// package com.vizz.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+// @Configuration
+// @EnableWebSecurity
+// public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsServiceImpl userDetailsServiceImpl;
+//     @Autowired
+//     UserDetailsServiceImpl userDetailsServiceImpl;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
-    }
+//     @Override
+//     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//         auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+//     }
 
-    @Override
-    protected void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws java.lang.Exception {
-        http.authorizeRequests().antMatchers("/", "/login").permitAll()
-        .antMatchers("/home").authenticated()
-        .and().csrf().disable()
-        .formLogin().loginPage("/login")
-        .defaultSuccessUrl("/home")
-        .usernameParameter("username")
-        .passwordParameter("password")
-        .and().exceptionHandling().accessDeniedPage("/access-denied")
-        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
-    }
+//     @Override
+//     protected void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws java.lang.Exception {
+//         http.authorizeRequests().antMatchers("/", "/login").permitAll()
+//         .antMatchers("/home").authenticated()
+//         .and().csrf().disable()
+//         .formLogin().loginPage("/login")
+//         .defaultSuccessUrl("/home")
+//         .usernameParameter("username")
+//         .passwordParameter("password")
+//         .and().exceptionHandling().accessDeniedPage("/access-denied")
+//         .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+//     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//     @Bean
+//     public BCryptPasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
     
-}
+// }
