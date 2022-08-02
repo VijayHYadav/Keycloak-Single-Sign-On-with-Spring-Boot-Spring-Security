546b774530789508c349309c88bb02e0823a4bd9
VijayHYadav <vizzyadav1997@gmail.com>
2022-08-02 23:39:39 +0530

    11. Access Denied Page with Spring Security

--- src/main/java/com/vizz/controllers/StudentController.java
+++ src/main/java/com/vizz/controllers/StudentController.java
@@ -28,4 +28,10 @@ public class StudentController {
         ModelAndView modelAndView = new ModelAndView("manage-students");
         return modelAndView;
     }
+
+    @GetMapping("/access-denied")
+    public ModelAndView accessDenied() {
+        ModelAndView modelAndView = new ModelAndView("access-denied");
+        return modelAndView;
+    }
 }
--- src/main/java/com/vizz/security/SpringSecurityConfig.java
+++ src/main/java/com/vizz/security/SpringSecurityConfig.java
@@ -29,7 +29,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin().loginPage("/login")
         .defaultSuccessUrl("/home")
         .usernameParameter("username")
-        .passwordParameter("password");
+        .passwordParameter("password")
+        .and().exceptionHandling().accessDeniedPage("/access-denied");
     }
 
     @Bean
